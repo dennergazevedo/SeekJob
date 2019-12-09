581ec60167a49d32e4684e4c8ac103598b211417
dennergazevedo <54537119+dennergazevedo@users.noreply.github.com>
2019-12-09 12:00:46 -0300

    Inicio do Projeto

---
+++ constants/articles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: 'E ta pronto o sorvetinho',
+    image: 'https://pbs.twimg.com/media/DVUdBCaWsAARn3K.jpg',
+    cta: 'Ver mais', 
+    horizontal: true
+  },
+  {
+    title: 'Putinhas',
+    image: 'https://pbs.twimg.com/media/EB3dKGiXUAA8Gq0.jpg',
+    cta: 'Ver mais'
+  },
+  {
+    title: 'Maikeu Jeber',
+    image: 'http://www.e-farsas.com/wp-content/uploads/ayu5.jpg',
+    cta: 'Ver mais'
+  },
+  {
+    title: 'Thata Delicia',
+    image: 'https://i.imgur.com/ciBynWf.jpg',
+    cta: 'Ver mais'
+  },
+  {
+    title: 'Lucifer',
+    image: 'https://hips.hearstapps.com/vidthumb/images/lucifer-season-4-trailer-still-1556724270.jpg?crop=0.740xw:1.00xh;0.175xw,0&resize=480:*',
+    cta: 'Ver mais', 
+    horizontal: true
+  },
+];
---
+++ screens/Login.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Input from '../components/Input';
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Dimensions
+} from "react-native";
+import { Block, Button, Text, theme, Icon } from "galio-framework";
+
+const { height, width } = Dimensions.get("screen");
+
+import argonTheme from "../constants/Theme";
+import Images from "../constants/Images";
+
+class Onboarding extends React.Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <Block flex style={styles.container}>
+        <StatusBar hidden />
+        <Block flex center>
+        <ImageBackground
+            source={Images.Onboarding}
+            style={{ height, width, zIndex: 1 }}
+          />
+        </Block>
+        <Block center>
+          <Image source={Images.LogoOnboarding} style={styles.logo} />
+        </Block>
+        <Block flex space="between" style={styles.padded}>
+            <Block flex space="around" style={{ zIndex: 2 }}>
+              <Block style={styles.title}>
+                <Block>
+                  <Text color="white" size={35} >
+                    
+                  </Text>
+                </Block>
+
+                <Block style={styles.subTitle}>
+                  <Text color="white" size={14} onPress={() => navigation.navigate("Home")}>
+                    Esqueceu sua senha?
+                  </Text>
+                </Block>
+
+                <Block>
+                  <Input 
+                    placeholder=" E-mail"
+                    iconContent={
+                      <Icon
+                        size={14}
+                        color={argonTheme.COLORS.ICON}
+                        name="user"
+                        family="AntDesign"
+                      />
+                    }
+                  />
+                </Block>
+                <Block>
+                  <Input 
+                    placeholder=" Senha"
+                    iconContent={
+                      <Icon
+                        size={14}
+                        color={argonTheme.COLORS.ICON}
+                        name="lock"
+                        family="AntDesign"
+                      />
+                    }
+                  />
+                </Block>
+
+                  <Block center>
+                    <Button
+                      style={styles.button}
+                      color={argonTheme.COLORS.SECONDARY}
+                      onPress={() => navigation.navigate("Home")}
+                      textStyle={{ color: argonTheme.COLORS.BLACK }}
+                    >
+                      Entrar
+                    </Button>
+                  </Block>
+                </Block>
+              </Block>
+          </Block>
+        </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.COLORS.BLACK,
+  },
+  padded: {
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    position: "relative",
+    bottom: theme.SIZES.BASE,
+    zIndex: 2,
+  },
+  button: {
+    marginTop: 10,
+    width: width - theme.SIZES.BASE * 12,
+    height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0,
+    borderRadius: 100
+  },
+  logo: {
+    width: 120,
+    height: 220,
+    zIndex: 2,
+    position: 'relative',
+    marginTop: '-60%'
+  },
+
+  title: {
+    marginTop:'-5%'
+  },
+  subTitle: {
+    marginTop: 5,
+    marginLeft: 190,
+  }
+});
+
+export default Onboarding;
